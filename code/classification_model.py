a63262222929a94c2129ac5f33785f312ae5e38d
Mark Anderson <andersm3@tcd.ie>
2020-07-06 22:57:21 +0000

    CRNN initial commit

--- code/classification_model.py
+++ code/classification_model.py
@@ -3,23 +3,27 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, Dense, Dropout, Flatten, MaxPooling2D
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-tf.executing_eagerly()
+#tf.executing_eagerly()
+#physical_devices = tf.config.list_physical_devices('GPU')
+#try:
+#    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+#except:
+#    pass
 
 from datetime import datetime
 import argparse
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import crnn
 
 def opts_parser():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-t', '--type', type=str, default='linear')
-    parser.add_argument('-lr', '--learning-rate', type=float, default=0.001)
+    parser.add_argument('-lr', '--learning-rate', type=float, default=0.0001)
     parser.add_argument('-e', '--epochs', type=int, default=20)
 
     return parser
@@ -41,11 +45,11 @@ def main():
     if spect_type == 'linear':
         IMAGE_HEIGHT = 257
         IMAGE_WIDTH = 1000
-        BATCH_SIZE = 64
+        BATCH_SIZE = 128
     else:
         IMAGE_HEIGHT = 160
         IMAGE_WIDTH = 998
-        BATCH_SIZE = 64
+        BATCH_SIZE = 32 
 
     image_generator = ImageDataGenerator(validation_split=0.2, rescale=1./255)
 
@@ -70,24 +74,7 @@ def main():
     logdir = "../logs/" + datetime.now().strftime("%Y%m%d-%H%M%S")
     tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
 
-    model = Sequential([
-        Conv2D(16, 5, padding='valid', activation='relu', input_shape=(IMAGE_HEIGHT, IMAGE_WIDTH, 3)),
-        MaxPooling2D(),
-        Dropout(0.2),
-        Conv2D(32, 5, padding='valid', activation='relu'),
-        MaxPooling2D(),
-        Dropout(0.2),
-        Conv2D(64, 5, padding='valid', activation='relu'),
-        MaxPooling2D(),
-        Dropout(0.2),
-        Conv2D(128, 5, padding='valid', activation='relu'),
-        MaxPooling2D(),
-        Flatten(),
-        Dense(256, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(l=learning_rate)),
-        Dropout(0.2),
-        Dense(32, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(l=learning_rate)),
-        Dense(1, activation='sigmoid')
-    ])
+    model = crnn.CRNN2D([IMAGE_HEIGHT, IMAGE_WIDTH, 3], 1)
 
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy', tf.keras.metrics.AUC()])
     model.summary()
---
+++ code/crnn.py
@@ -0,0 +1,53 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Reshape, Permute
+from tensorflow.keras.layers import Conv1D, Conv2D, MaxPooling1D, MaxPooling2D
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import GRU, LSTM
+
+def CRNN2D(shape, n_classes):
+    n_layers = 4
+    n_filters = [32, 64, 128, 128]
+    kernel_size = (3, 3)
+    activation = 'relu'
+    pool_size = [(2, 2), (4, 2), (4, 2), (4, 2)]
+
+    input_shape = (shape[0], shape[1], shape[2])
+    frequency_axis = 1
+    time_axis = 2
+    channel_axis = 3
+
+    # create sequential model
+    model = Sequential()
+    model.add(BatchNormalization(axis=frequency_axis, input_shape=input_shape))
+    
+    # first conv layer with shape params
+    model.add(Conv2D(n_filters[0], kernel_size=kernel_size, padding='same'))
+    model.add(Activation(activation))
+    model.add(BatchNormalization(axis=channel_axis))
+    model.add(MaxPooling2D(pool_size=pool_size[0], strides=pool_size[0]))
+    model.add(Dropout(0.1))
+
+    # other convolutional layers (for loop)
+    for layer in range(n_layers - 1):
+        model.add(Conv2D(n_filters[layer+1], kernel_size=kernel_size, padding='same'))
+        model.add(Activation(activation))
+        model.add(BatchNormalization(axis=channel_axis))
+        model.add(MaxPooling2D(pool_size=pool_size[layer+1], strides=pool_size[layer+1]))
+        model.add(Dropout(0.1))
+
+    # reshape for R layers
+    # flip frequency and time axes
+    model.add(Permute((time_axis, frequency_axis, channel_axis)))
+    resize_shape = model.output_shape[2] * model.output_shape[3]
+    model.add(Reshape((model.output_shape[1], resize_shape)))
+
+    # recurrent layer
+    model.add(GRU(32, return_sequences=True))
+    model.add(GRU(32, return_sequences=False))
+    model.add(Dropout(0.3))
+
+    # Dense Layers
+    model.add(Dense(n_classes))
+    model.add(Activation(activation))
+
+    return model
